2538c237326e86cc1bbc1014f15b530bc3e34b02
Nikolay Karataev <babaka@cinemood.com>
2017-09-02 19:31:25 -0400

    fixes… again

--- CMDFramework/Framework/UI/Buttons/CMDBackButton.swift
+++ CMDFramework/Framework/UI/Buttons/CMDBackButton.swift
@@ -14,7 +14,7 @@ open class CMDBackButton: CMDClassicButton {
         self.text = !DeviceType.IS_IPHONE_5_OR_LESS ? "CANCEL".podLocalization : ""
         self.setTitleColor(UIColor.buttons.backButtonTitle, for: .normal)
         self.titleEdgeInsets = UIEdgeInsets(top: 0.0, left: 10.0, bottom: 0.0, right: 0.0)
-        self.setImage(getImageFromBundle(name: "back", withClass: self), for: .normal)
-        self.setImage(getImageFromBundle(name: "back", withClass: self), for: .highlighted)
+        self.setImage(getImageFromBundle(name: "back", withClass: CMDBackButton.self), for: .normal)
+        self.setImage(getImageFromBundle(name: "back", withClass: CMDBackButoon.self), for: .highlighted)
     }
 }
